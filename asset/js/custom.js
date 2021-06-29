01a1184809d3ae50f307d9d17360d16c44803700
marlara <lara.marzi@gmail.com>
2021-06-29 16:32:14 +0200

    more changes

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -78,32 +78,37 @@ $(document).ready(function() {
 //slider
 
 $(document).ready(function(){
-    $('.slider-block').append('<div class="pagination-slider"></div>') //add the div for the pagination
 
-    var titles_pagination = $('.slider-title');
-    titles_pagination.each(function(){
-        $('.pagination-slider').append('<div class="select-image" alt="go to image">'+$(this).text()+"</div>") //gets the titles and relative urls to the slider items
+    $('.slider').each(function(){
+        $slider = $(this);
+        var $slider_block = $slider.find($('.slider-block'));
+        $slider_block.append('<div class="pagination-slider"></div>') //add the div for the pagination
+
+        var titles_pagination = $slider.find($('.slider-title'));
+        titles_pagination.each(function(){
+            $slider.find($('.pagination-slider')).append('<div class="select-image" alt="go to image">'+$(this).text()+"</div>") //gets the titles and relative urls to the slider items
+        });
+
+        $slider.find($('.imageslide')).first().addClass('current').show();
+        $slider.find($('.select-image')).first().addClass('selected');
+        
+        $slider.find($('.imageslide')).each(function( i, el ){ //add a numbered id for each figure
+            $(el).attr('id','image-num-'+i);
+        });
+
+        var $slides = $slider.find($('.imageslide')); 
+
+        $slider.find($('.select-image')).click(function() {
+            var index = $(this).index(); //the index of the selector
+            var $nextSlide = $slides.eq(index);
+            // don't do anything if same indexed slide already has current class
+            if (!$nextSlide.hasClass('current')) {
+                $slides.filter('.current').removeClass('current').fadeOut(function() {
+                $('.select-image').filter('.selected').removeClass('selected')
+                $nextSlide.addClass('current').fadeIn()
+                $('.select-image').eq(index).addClass('selected');
+            });
+            }
+        });
     });
-
-    $('.imageslide:first').addClass('current').show();
-	$('.select-image:first').addClass('selected');
-    
-    $('.imageslide').each(function( i, el ){ //add a numbered id for each figure
-        $(el).attr('id','image-num-'+i);
-    });
-	
-    var $slides = $('.imageslide');
-
-    $('.select-image').click(function() {
-        var index = $(this).index(); //the index of the selector
-        var $nextSlide = $slides.eq(index);
-        // don't do anything if same indexed slide already has current class
-        if (!$nextSlide.hasClass('current')) {
-            $slides.filter('.current').removeClass('current').fadeOut(function() {
-            $('.select-image').filter('.selected').removeClass('selected')
-            $nextSlide.addClass('current').fadeIn()
-            $('.select-image').eq(index).addClass('selected');
-          });
-        }
-      });
 });
